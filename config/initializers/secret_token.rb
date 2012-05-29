32c95ad4b4a35150b1452e68bcb18355c96e35b6
Abdul-Mutaal H Bilal <mutaalbilal@yahoo.com>
2012-05-29 13:55:44 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CdwRailsApp::Application.config.secret_token = 'dfd2e6cc68b71b62168f2fa3a7bc66e2d60f607e9bf32fafe9ce46c79dbd02df830371fc86984eef0998b02c598763ac8122073687dcdd27fe44be228bc08416'
